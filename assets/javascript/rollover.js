55d7eb6d69d8cd71e3c3efd1e25790b297d12222
kvitorr <kvitorrsantos@gmail.com>
2023-02-09 07:47:09 -0300

    att

---
+++ assets/javascript/rollover.js
@@ -0,0 +1,63 @@
+const vitor = document.getElementById("vitor")
+const adelia = document.getElementById("adelia")
+const marcos = document.getElementById("marcos")
+const vitorimg = document.getElementById("vitor-img")
+const adeliaimg = document.getElementById("adelia-img")
+const marcosimg = document.getElementById("marcos-img")
+
+vitorimg.addEventListener("mouseover", () => {
+    
+    vitor.style.opacity = 0;
+
+    setTimeout(() => {
+        vitor.setAttribute('src', './assets/images/vitor.jpeg')
+        vitor.style.opacity = 1;
+    }, 500);
+})
+
+vitorimg.addEventListener("mouseout", () => {
+    vitor.style.opacity = 0;
+    setTimeout(() => {
+      vitor.setAttribute('src', './assets/images/download20230203205623.png');
+      vitor.style.opacity = 1;
+    }, 500);
+})
+
+
+
+marcosimg.addEventListener("mouseover", () => {
+    
+    marcos.style.opacity = 0;
+
+    setTimeout(() => {
+        marcos.setAttribute('src', './assets/images/marcos.jpeg')
+        marcos.style.opacity = 1;
+    }, 500);
+})
+
+marcosimg.addEventListener("mouseout", () => {
+    marcos.style.opacity = 0;
+    setTimeout(() => {
+      marcos.setAttribute('src', './assets/images/317007774_1065445031525087_3766872975051679921_n.jpg');
+      marcos.style.opacity = 1;
+    }, 500);
+})
+
+
+adeliaimg.addEventListener("mouseover", () => {
+    
+    adelia.style.opacity = 0;
+
+    setTimeout(() => {
+        adelia.setAttribute('src', './assets/images/adelia.jpeg')
+        adelia.style.opacity = 1;
+    }, 500);
+})
+
+adeliaimg.addEventListener("mouseout", () => {
+    adelia.style.opacity = 0;
+    setTimeout(() => {
+      adelia.setAttribute('src', './assets/images/WhatsApp Image 2023-02-08 at 21.35.00.jpeg');
+      adelia.style.opacity = 1;
+    }, 500);
+})
